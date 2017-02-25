bf1d7df71f17d96a49aca6d1ea4640c6cbfc41e2
anderson_dario <andersondotcollection@gmail.com>
2017-02-25 19:50:53 -0300

    Finish the tutorial

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
 
+// DB connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+if (process.env.NODE_ENV !== 'test') {
+    mongoose.connect('mongodb://localhost/muber');
+}
 
+// Middlewares
 app.use(bodyParser.json());
 
+// Routes
 routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422)
+    .send({ error: err.message });
+});
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,10 @@ module.exports = (app) => {
   // Watch for incoming requests of method GET
   // to the route http:://localhost:3050/api
   app.get('/api', DriversController.greeting);
+
+  // Drivers routes
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
+  app.delete('/api/drivers/:id', DriversController.delete);
+  app.get('/api/drivers', DriversController.index);
 };
